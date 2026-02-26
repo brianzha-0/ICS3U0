19155f0b1fb174975208d4fe8b4ba2191077d282
BrianZhao10 <brianzhaoptimal@gmail.com>
2026-02-26 15:43:24 -0500

    Update PST research document as well as format links into IEEE citations, keeping them up-to-date; create directory for programming work to be tracked inside of and add Lab-00

---
+++ Programming Labs' Solutions/main.py
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------
+# Name:        Introduction (main.py)
+# Purpose:     Practicing basic skills in Python
+#
+# Author:      Brian Z.
+# Created:     26-Feb-2026
+# Updated:     26-Feb-2026
+#-----------------------------------------------------------------------------
+
+print("Hello World!")
+
+# I have known for quite a while that the programming language called Python is a popular, powerful coding tool. Because of this, I am ecstatic to begin learning how to hone the concepts taught and create cool things with it; I am on the edge of my seat in anticipation for reading and going at the upcoming lesson(s).
